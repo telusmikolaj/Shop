a2c8dc11e034950e421a92473b3e5fa67e28f175
Mikołaj Telus <mikolaj.telus@gmail.com>
2022-10-13 16:42:55 +0000

    Merged in feature_telusmikolaj_SHOP-3_init_product_data (pull request #11)
    feature_telusmikolaj_SHOP-3 INIT DATA
    
    * SHOP-3 INIT DATA
    
    
    Approved-by: Paweł Szczepkowski

---
+++ src/main/java/pl/waw/great/shop/configuration/TestDataLoader.java
@@ -0,0 +1,40 @@
+package pl.waw.great.shop.configuration;
+
+import org.apache.commons.math3.random.RandomDataGenerator;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import pl.waw.great.shop.model.Product;
+import pl.waw.great.shop.repository.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
+
+@Component
+@Profile("dev")
+public class TestDataLoader {
+
+    private final ProductRepository productRepository;
+    private final RandomDataGenerator randomDataGenerator;
+
+    public TestDataLoader(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+        this.randomDataGenerator = new RandomDataGenerator();
+        generateTestProducts();
+    }
+
+    public void generateTestProducts() {
+        List<Product> productList = IntStream.range(0, 1000)
+                .mapToObj(product -> new Product(
+                        randomAlphanumeric(randomDataGenerator.nextInt(2, 10)),
+                        randomAlphanumeric(randomDataGenerator.nextInt(2, 20)),
+                        BigDecimal.valueOf(Long.parseLong(randomNumeric(1, 5)))
+                )).collect(Collectors.toList());
+
+        productList.forEach(this.productRepository::createProduct);
+    }
+}
